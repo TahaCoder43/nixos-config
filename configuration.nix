77534ebd14d2858f3011e9ff4ba1c8fb91b9568f
Taha ibn Munawar <taha-ibn-munawar@proton.me>
2025-01-19 14:01:27 +0500

    Initial commit with long configuration

---
+++ configuration.nix
@@ -0,0 +1,274 @@
+# Edit this configuration file to define what should be installed on your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+let
+  unstable = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz) { config.allowUnfree = true;};
+  mypkgs = {
+    fonts = {
+      serenity-os-emoji = pkgs.stdenvNoCC.mkDerivation rec {
+        pname = "serenity-os-emoji";
+        version = "1.0";
+
+        src = pkgs.fetchurl {
+          url = "https://linusg.github.io/serenityos-emoji-font/SerenityOS-Emoji.ttf";
+          hash = "sha256-YvI9EEhEzw29ibcCh1H+bkVJtODHQQe88xsMU90wKVg=";
+        };
+
+        dontUnpack = true;
+
+        installPhase = ''
+          runHook preInstall
+          echo debugging
+          install -Dm644 $src -t $out/share/fonts/truetype
+          runHook postInstall
+        '';
+      };
+    };
+    tmux-plugin-manager = pkgs.tmuxPlugins.mkTmuxPlugin rec {
+      pluginName = "tpm";
+      version = "3.1.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "tmux-plugins";
+        repo = "tpm";
+        tag = "v${version}";
+        hash = "sha256-CeI9Wq6tHqV68woE11lIY4cLoNY8XWyXyMHTDmFKJKI=";
+      };
+    };
+  };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking = { 
+    nameservers = [ "1.1.1.1" ]; # Set static dns to cloud flare
+    hostName = "nixos";
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        General = {
+          EnableNetworkConfiguration=true; # Use iwd built in dhcp client
+        };
+        Network = {
+          NameResolvingService="none"; # Prevent dynamic dns
+        };
+      };
+    };
+    dhcpcd.enable = false;
+    resolvconf.enable = false; # enabled by default and overrides network.nameservers with dynamic dns
+    useDHCP = false;
+    interfaces.wlan0 = { # iwd uses wlan0 instead of wlp2s0
+      useDHCP = true; # The recommended way is to enable dhcp like this
+    };
+  };
+
+  # Set your time zone.
+  time.timeZone = "Asia/Karachi";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ur_PK";
+    LC_IDENTIFICATION = "ur_PK";
+    LC_MEASUREMENT = "ur_PK";
+    LC_MONETARY = "ur_PK";
+    LC_NAME = "ur_PK";
+    LC_NUMERIC = "ur_PK";
+    LC_PAPER = "ur_PK";
+    LC_TELEPHONE = "ur_PK";
+    LC_TIME = "ur_PK";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.taham = {
+    isNormalUser = true;
+    description = "Taha ibn Munawar";
+    extraGroups = [
+#      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      go-2fa
+    ];
+  };
+
+  users.groups = {
+    ydotool.members = [ "taham" ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.etc = {
+    "rofi/themes".source = "${pkgs.rofi-wayland}/share/rofi/themes";
+  };
+
+  environment.systemPackages = with pkgs; [
+    # install activity watch, keyviz, and flameshot now
+    python312
+    unstable.python312Packages.notebook
+    nodejs_23
+    gcc
+    gnumake
+
+    vim 
+    neovim
+    btop
+    gitui
+    oh-my-posh
+    # mypkgs.gauth
+
+    zsh
+    kitty
+    foot
+    tmux
+    mypkgs.tmux-plugin-manager
+
+    git
+    wget
+    ripgrep # grep alternative
+    libinput # to debug input events
+    pciutils # provides lspci command to show usb device information
+    exiftool
+    ntfs3g # Required to be able to work with ntfs file system
+    smartmontools # hard disk smart test runner
+    fzf
+    jq
+    ruff # python linter
+    dig 
+    iftop
+
+    microsoft-edge
+    unstable.inkscape
+    gimp
+    filelight
+    sqlitebrowser
+    unstable.activitywatch # for some reason stable version failed to build, meanwhile unstable succeded ????
+    awatcher
+        # libsForQt5.qt5.qtwayland
+  ];
+
+  programs.sway = {
+    enable = true;
+    #wrappersFeatures.gtk = true; if gtk fails turn this on 
+    # figure out whether to use this or .zprofile
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export QT_QPA_PLATFORM=wayland
+    '';
+    extraPackages = with pkgs; [
+      # might consider deleting these
+      dmenu
+      sway-contrib.grimshot
+
+      rofi-wayland
+      flameshot
+      slurp
+      wf-recorder
+      ydotool
+      wl-clipboard
+      swaynotificationcenter
+    ];
+  };
+
+  programs.git.config = {
+    init.defaultBranch = "main";
+    user = {
+      email = "taha-ibn-munawar@proton.me";
+      name = "Taha ibn Munawar";
+    };
+  };
+  programs.tmux =  {
+    enable = true;
+    extraConfig = ''
+      run ${mypkgs.tmux-plugin-manager}/share/tmux-plugins/tpm/tpm
+    '';
+  };
+  programs.zsh.enable = true;
+  programs.firefox.enable = true;
+  programs.ssh.startAgent = true;
+  programs.ydotool.enable = true;
+
+  fonts = {
+    packages = with pkgs; [
+      poppins
+      unstable.nerd-fonts.iosevka
+      mypkgs.fonts.serenity-os-emoji
+      google-fonts # all google fonts downloaded, good for designing
+    ];
+    fontconfig.defaultFonts = {
+      monospace = [ "Iosevka Nerd Font" ];
+      sansSerif = [ "Poppins" ];
+      emoji = [ "SerenityOS Emoji" ];
+    };
+  };
+
+  # Configure keymap in X11
+  # Not working :'(
+  services.xserver.xkb = {
+    layout = "us,ara-ph";
+    options = "keypad:pointerkeys,lv3:caps_switch,grp:rctrl_toggle";
+    extraLayouts.ara-ph = {
+      description = "Arabic layout that is phonetically mapped to english";
+      languages = [ "ara" ];
+      symbolsFile = /home/taham/.layouts/ara-ph;
+    };
+  };
+
+  # Sound
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true; # if not already enabled
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  
+    # hardware.pulseaudio.enable = true;
+    #hardware.pulseaudio.support32Bit = true;    ## If compatibility with 32-bit applications is desired.
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}

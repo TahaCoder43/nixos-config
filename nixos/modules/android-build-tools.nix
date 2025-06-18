413cdd937e9e95f581fbd7e37381d58cccbb6080
Taha Munawar <Taha-ibn-Munawar@proton.me>
2025-06-18 16:09:01 +0500

    Applying a fix provided by redditor

--- nixos/modules/android-build-tools.nix
+++ nixos/modules/android-build-tools.nix
@@ -17,21 +17,22 @@ let
       pkgs.pkg-config
     ];
 
-    autoPatchelfIgnoreMissingDeps = [ "libc++.so" ];
-
     buildInputs = [
       pkgs.libgcc
-      # pkgs.libcxx
-      pkgs.llvmPackages_19.libcxxClang
-      # pkgs.llvmPackages_19.clangUseLLVM
+      pkgs.libcxx
     ];
 
     dontBuild = true;
 
+    preFixup = ''
+      patchelf --replace-needed libc++.so libc++.so.1 $out/bin/zipalign
+      patchelf --add-needed libc++abi.so.1 $out/bin/zipalign
+    '';
+
     installPhase = ''
       runHook preInstall
-      install -Dm744 $src/zipalign -t $out/
-      install -Dm744 $src/apksigner -t $out/
+      install -Dm744 $src/zipalign -t $out/bin/
+      install -Dm744 $src/apksigner -t $out/bin/
       runHook postInstall
     '';
   };

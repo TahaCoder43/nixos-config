d9f7d781adfe26e60e838c4bd70b6dc0c7470cdb
Taha Munawar <Taha-ibn-Munawar@proton.me>
2026-01-30 00:37:48 +0500

    Updating configuration.nix (likely)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -218,7 +218,7 @@ in
     swaynotificationcenter
     slurp
     unstable.activitywatch # for some reason stable version failed to build, meanwhile unstable succeded ????
-    pkgs-unstable.python314Packages.notebook
+    pkgs-unstable.python314Packages.jupyterlab
     unstable.mcpelauncher-ui-qt
     unstable.inkscape
     vlc

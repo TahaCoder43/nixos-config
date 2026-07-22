8a58529ea09041a914c852cc2037530d2fba4566
Taha ibn Munawar <taha-ibn-munawar@proton.me>
2026-07-22 06:40:25 +0500

    replacing fn with capslock

--- nixos/modules/keyd.nix
+++ nixos/modules/keyd.nix
@@ -15,8 +15,29 @@
       ids = [ "*" ];
       settings = {
         main = {
-          capslock = "layer(fn)";
           fn = "control";
+          capslock = "overload(capslock_layer, esc)";
+        };
+        capslock_layer = {
+          # Audio Controls
+          f1 = "mute";
+          f2 = "volumedown";
+          f3 = "volumeup";
+          f4 = "micmute";
+
+          # Display Brightness
+          f5 = "brightnessdown";
+          f6 = "brightnessup";
+
+          # Display / Connectivity
+          f7 = "displaytoggle";
+          f8 = "wlan";
+
+          # Media & Settings
+          f9 = "config";
+          f10 = "previoussong";
+          f11 = "playpause";
+          f12 = "nextsong";
         };
       };
     };
@@ -24,50 +45,4 @@
   systemd.services.keyd.serviceConfig.CapabilityBoundingSet = [
     "CAP_SETGID" # see this issue https://github.com/NixOS/nixpkgs/issues/290161
   ];
-
-  # environment.etc = {
-  #   "keyd/hp-gaming-keyboard-fix.conf".text = ''
-  #     # Base layer (default state)
-  #     [ids]
-  #
-  #     1a2c:9df4
-  #
-  #     [main]
-  #
-  #     3 = oneshot(keyfix-3)
-  #     e = oneshot(keyfix-e)
-  #     s = oneshot(keyfix-s)
-  #     x = oneshot(keyfix-x)
-  #     leftalt = oneshot(keyfix-leftalt)
-  #     capslock = layer(nav)
-  #
-  #     [nav]
-  #
-  #     h = left
-  #     j = down
-  #     k = up
-  #     l = right
-  #
-  #     [keyfix-3]
-  #
-  #     backspace = 3
-  #
-  #     [keyfix-e]
-  #
-  #     \ = e
-  #
-  #     [keyfix-s]
-  #
-  #     enter = s
-  #
-  #     [keyfix-x]
-  #
-  #     rightshift = x
-  #
-  #     [keyfix-leftalt]
-  #
-  #     rightctrl = leftalt
-  #
-  #   '';
-  # };
 }
